05bffc7741beac03d06ad61fb071aec243ee007d
Vojtech Juranek <vojtech.juranek@gmail.com>
2017-05-28 22:05:02 +0200

    Start with integration tests

---
+++ malaria/malaria-server/src/test/java/eu/imagecode/scias/util/DeploymentBuilder.java
@@ -0,0 +1,31 @@
+package eu.imagecode.scias.util;
+
+import java.io.File;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import eu.imagecode.scias.service.AnalysisService;
+import eu.imagecode.scias.service.BatchService;
+
+public class DeploymentBuilder {
+
+    public static WebArchive createRestServerWar() {
+        
+        JavaArchive jpaJar = ShrinkWrap.createFromZipFile(JavaArchive.class, new File("target/test-libs/malaria-jpa.jar"));
+        jpaJar.delete("/META-INF/persistence.xml");
+        jpaJar.addAsManifestResource(new File("src/test/resources/META-INF/persistence.xml"));
+        
+        return ShrinkWrap.create(WebArchive.class)
+                .addAsLibraries(
+                    new File("target/test-libs/malaria-rest.jar"))
+                .addAsLibrary(jpaJar)
+                .addClass(AnalysisService.class)
+                .addClass(BatchService.class)
+                .addPackage("eu.imagecode.scias.util")
+                .addPackage("eu.imagecode.scias.testutil")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+}
---
+++ malaria/malaria-server/src/test/resources/scripts/populate_db.sql
@@ -0,0 +1,4 @@
+-- INSERT DATA
+INSERT INTO station(id, uuid, name) VALUES(1, 'aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb', 'testStation');
+INSERT INTO station(id, uuid, name) VALUES(2, 'f8ffba00-9134-4828-b34d-c03b4b2ee736', 'testStation2');
+
