63e24490873beb4fd8f6e8e95b2ae56b3da3c09f
Vojtech Juranek <vojtech.juranek@gmail.com>
2017-07-14 09:05:25 +0200

    #31 Add logging into scias common module

--- scias/scias-common/src/main/java/eu/imagecode/scias/service/ImageService.java
+++ scias/scias-common/src/main/java/eu/imagecode/scias/service/ImageService.java
@@ -6,13 +6,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import eu.imagecode.scias.util.SciasFunctions;
 
 @Stateless
 public class ImageService {
+
+    @Inject
+    Logger log;
 
     private static final int BUFFER_SIZE = 1024; // TODO put into some global configuration?
     private static final String HASH_ALG = "SHA-256";
@@ -21,7 +26,11 @@ public class ImageService {
                     throws IOException, IllegalStateException, NoSuchAlgorithmException {
         String uploadedSHA = SciasFunctions.digestToString(copyStream(uploadedFile, imageStream));
 
+        log.fine(String.format("File '%s': expected hash '%s', actual hash '%s'", uploadedFile.getName(),
+                        expectedSHA256, uploadedSHA));
         if (!expectedSHA256.equals(uploadedSHA)) {
+            log.warning(String.format("File '%s': expected hash (%s) is not the same as hash of uploade bits (%s)",
+                            uploadedFile.getName(), expectedSHA256, uploadedSHA));
             throw new IllegalStateException(String.format("SHA256 doesn match! Expected: %s, uploaded: %s",
                             expectedSHA256, uploadedSHA));
         }
