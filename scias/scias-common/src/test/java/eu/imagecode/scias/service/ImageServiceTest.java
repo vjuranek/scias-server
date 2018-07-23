e52775727e1f50b6596287d3c8a422c705911b46
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-07-23 15:44:26 +0200

    #50 Fix failing ImageService test

--- scias/scias-common/src/test/java/eu/imagecode/scias/service/ImageServiceTest.java
+++ scias/scias-common/src/test/java/eu/imagecode/scias/service/ImageServiceTest.java
@@ -4,14 +4,17 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.security.MessageDigest;
+import java.util.logging.Logger;
 
+import org.jboss.weld.junit4.WeldInitiator;
+import org.junit.Rule;
 import org.junit.Test;
 
+import eu.imagecode.scias.util.Resources;
 import eu.imagecode.scias.util.SciasFunctions;
 
 public class ImageServiceTest {
@@ -19,12 +22,15 @@ public class ImageServiceTest {
     public static final String TEST_IMG1 = "img/Image01.jpg";
     public static final String IMG1_SHA256 = "7172698439171a96716f2190061f214c55a22feadafa460ca5b8b8e2ebf6482e";
 
+    @Rule
+    public WeldInitiator weld = WeldInitiator.from(ImageService.class, Resources.class, Logger.class).build();
+    
     @Test
     public void testUploadImage() throws Exception {
         InputStream in = ImageServiceTest.class.getClassLoader().getResourceAsStream(TEST_IMG1);
         File out = File.createTempFile("tmp-test-", ".jpg");
 
-        ImageService imgService = new ImageService();
+        ImageService imgService = weld.select(ImageService.class).get();
         imgService.uploadImage(IMG1_SHA256, out, in);
 
         byte[] inBytes = Files.readAllBytes(FileSystems.getDefault()
@@ -45,7 +51,7 @@ public class ImageServiceTest {
         String sha256 = "blablabla";
         File out = File.createTempFile("tmp-test-", ".jpg");
 
-        ImageService imgService = new ImageService();
+        ImageService imgService = weld.select(ImageService.class).get();
         imgService.uploadImage(sha256, out, in);  
     }
 
