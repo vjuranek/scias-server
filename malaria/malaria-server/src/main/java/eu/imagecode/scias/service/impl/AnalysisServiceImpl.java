43dad96f80d73910226b10ce0ca009e34b7325c1
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-08-08 14:28:15 +0200

    #48 Remove comments from service impl

--- malaria/malaria-server/src/main/java/eu/imagecode/scias/service/impl/AnalysisServiceImpl.java
+++ malaria/malaria-server/src/main/java/eu/imagecode/scias/service/impl/AnalysisServiceImpl.java
@@ -14,7 +14,7 @@ import eu.imagecode.scias.model.jpa.AnalysisEntity;
 import eu.imagecode.scias.service.AnalysisService;
 
 /**
- * Service for manipulating with analyses.
+ * Implementation of {@link AnalysisService}
  * 
  * @author vjuranek
  *
@@ -25,70 +25,47 @@ public class AnalysisServiceImpl implements AnalysisService {
     @Inject
     private EntityManager em;
 
-    /**
-     * Loads all analyses from DB 
-     *
-     */
     public List<AnalysisEntity> getAllAnalyses() {
         return em.createNamedQuery(AnalysisEntity.QUERY_FIND_ALL, AnalysisEntity.class).getResultList();
     }
 
-    /**
-     * Loads analysis with given global/server ID 
-     *
-     */
     public AnalysisEntity getAnalysisById(int id) {
-        return em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_ID, AnalysisEntity.class).setParameter("analysisId", id).getSingleResult();
+        return em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_ID, AnalysisEntity.class).setParameter("analysisId", id)
+                        .getSingleResult();
     }
-    
-    /**
-     * Loads all analyses related to given batch ID
-     * 
-     */
+
     public List<AnalysisEntity> getAnalysisByBatchId(int batchId) {
         try {
-            return em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_BATCH_ID, AnalysisEntity.class).setParameter("batchId", batchId).getResultList();
+            return em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_BATCH_ID, AnalysisEntity.class)
+                            .setParameter("batchId", batchId).getResultList();
         } catch (NoResultException e) {
             return new ArrayList<AnalysisEntity>();
         }
     }
-    
-    /**
-     * Loads analysis with specified local/client ID for given station (local ID has to be unique for one station).
-     * 
-     */
+
     public AnalysisEntity getAnalysisByLocalId(int analysisId, int stationId) {
-        return em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_LOCAL_ID_AND_STATION, AnalysisEntity.class).setParameter("localId", analysisId).setParameter("stationID", stationId).getSingleResult();
+        return em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_LOCAL_ID_AND_STATION, AnalysisEntity.class)
+                        .setParameter("localId", analysisId).setParameter("stationID", stationId).getSingleResult();
     }
-    
-    /**
-     * Loads analysis with specified local/client ID for given station (local ID has to be unique for one station).
-     * 
-     */
+
     public AnalysisEntity getAnalysisByLocalId(int analysisId, String stationUuid) {
-        return em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_LOCAL_ID_AND_STATION_UUID, AnalysisEntity.class).setParameter("localId", analysisId).setParameter("stationUUID", stationUuid).getSingleResult();
+        return em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_LOCAL_ID_AND_STATION_UUID, AnalysisEntity.class)
+                        .setParameter("localId", analysisId).setParameter("stationUUID", stationUuid).getSingleResult();
     }
-    
-    /**
-     * Loads all analyses in specified time range.
-     * 
-     */
+
     public List<AnalysisEntity> getAnalysisInTimeRange(Timestamp from, Timestamp to) {
         try {
-            return em.createNamedQuery(AnalysisEntity.QUERY_FIND_IN_TIME_RANGE, AnalysisEntity.class).setParameter("from", from).setParameter("to", to).getResultList();
+            return em.createNamedQuery(AnalysisEntity.QUERY_FIND_IN_TIME_RANGE, AnalysisEntity.class)
+                            .setParameter("from", from).setParameter("to", to).getResultList();
         } catch (NoResultException e) {
             return new ArrayList<AnalysisEntity>();
         }
     }
-    
-    
-    /**
-     * Checks, whether analysis with given local/client ID was already uploaded from given station.
-     * 
-     */
+
     public boolean isAnalysisUploaded(int analysisId, int stationId) {
         try {
-            em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_LOCAL_ID_AND_STATION, AnalysisEntity.class).setParameter("localId", analysisId).setParameter("stationID", stationId).getSingleResult();
+            em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_LOCAL_ID_AND_STATION, AnalysisEntity.class)
+                            .setParameter("localId", analysisId).setParameter("stationID", stationId).getSingleResult();
         } catch (NoResultException e) {
             return false;
         } catch (NonUniqueResultException e) {
@@ -96,15 +73,12 @@ public class AnalysisServiceImpl implements AnalysisService {
         }
         return true;
     }
-    
-    
-    /**
-     * Checks, whether analysis with given local/client ID was already uploaded from given station.
-     * 
-     */
+
     public boolean isAnalysisUploaded(int analysisId, String stationUuid) {
         try {
-            em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_LOCAL_ID_AND_STATION_UUID, AnalysisEntity.class).setParameter("localId", analysisId).setParameter("stationUUID", stationUuid).getSingleResult();
+            em.createNamedQuery(AnalysisEntity.QUERY_FIND_BY_LOCAL_ID_AND_STATION_UUID, AnalysisEntity.class)
+                            .setParameter("localId", analysisId).setParameter("stationUUID", stationUuid)
+                            .getSingleResult();
         } catch (NoResultException e) {
             return false;
         } catch (NonUniqueResultException e) {
